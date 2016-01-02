367705cee40a07e6a0f2e8628e3e8a32a989878e
mediaintegrati <mediaintegrati@gmail.com>
2016-01-02 19:19:30 +0100

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-    var socket = io.connect('http://casa.wordpress-napoli.it:3702');
+    var socket = io.connect('qui ci va indirizzo seguito da :3702');
 
     /**
      * [Namespacing]
